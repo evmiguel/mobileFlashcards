a0fd1038380ce7ae24d81b1f280ae204ab8a8e10
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-30 12:39:40 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Decks from './components/Decks'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+      	<Decks />
+      </View>
+    );
+  }
+}
---
+++ components/Decks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class Decks extends Component {
+	render() {
+		return(
+			<View>
+				<Text>DECKS</Text>
+			</View>
+		)
+	}
+}
+
+export default Decks
