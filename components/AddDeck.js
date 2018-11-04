7e11bc51ae13c2ab425e564fc3f752e5cd0c8af7
Erika Miguel <ms.erikamiguel@gmail.com>
2018-11-04 11:23:25 -0500

    feature: building out add deck submission

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import TextButton from './TextButton'
 
-const AddDeck = (props) => {
-	return (
-		<View>
-			<Text>ADD DECK!</Text>
-		</View>
-	)
+class AddDeck extends Component {
+	state = {
+		deckName: ''
+	}
+
+	handleInput = (text) => {
+		this.setState({
+			deckName: text
+		})
+	}
+
+	submit = () => {
+		console.log('submit')
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>What is the title of your new deck?</Text>
+				<TextInput placeholder="DeckTitle"
+					onChangeText={(text) => this.handleInput(text)}/>
+				<TextButton
+					onPress={this.submit}
+					children='Create Deck'/>
+			</View>
+		)
+	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+})
 
 export default AddDeck
